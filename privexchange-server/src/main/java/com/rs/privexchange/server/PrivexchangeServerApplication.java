4426059cff4a304df0ab1ce5fce102323f2d956d
Rojar Smith <rojarsmith@gmail.com>
2024-07-21 23:42:28 +0800

    feat: Initial Server Project

---
+++ privexchange-server/src/main/java/com/rs/privexchange/server/PrivexchangeServerApplication.java
@@ -0,0 +1,13 @@
+package com.rs.privexchange.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivexchangeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivexchangeServerApplication.class, args);
+	}
+
+}
---
+++ privexchange-server/src/test/java/com/rs/privexchange/server/PrivexchangeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rs.privexchange.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivexchangeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
